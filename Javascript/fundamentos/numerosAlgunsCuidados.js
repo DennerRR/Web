76905f022890a79d7e1eab89574b3849ccd1bf14
Denner <dennerramiror@gmail.com>
2022-09-18 16:35:40 -0300

    Cuidados com numeros

---
+++ Javascript/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7/0)
+console.log("10"/2)// reconhece que 10 mesmo estando em string pode ser dividido por 2
+console.log(("Topzera!").length/2)//Dividindo o length por 2
+console.log(("Topzera!").endsWith("!")/2) // dividindo true por 2
+console.log(0.1+0.7)
+console.log((0.1+0.7).toFixed(2))
+//console.log(10.toString());
+console.log((10.456).toFixed(2))
