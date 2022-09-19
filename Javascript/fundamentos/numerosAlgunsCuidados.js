1becf797b445c444b82ba663cc9e0a3508704334
Denner <dennerramiror@gmail.com>
2022-09-19 17:56:32 -0300

    Update Cuidados com numeros file

--- Javascript/fundamentos/numerosAlgunsCuidados.js
+++ Javascript/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7/0)
 console.log("10"/2)// reconhece que 10 mesmo estando em string pode ser dividido por 2
+console.log("3" + 1);
 console.log(("Topzera!").length/2)//Dividindo o length por 2
 console.log(("Topzera!").endsWith("!")/2) // dividindo true por 2
 console.log(0.1+0.7)
