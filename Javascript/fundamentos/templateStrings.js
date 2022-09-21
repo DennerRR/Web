11808a18596b9f5e3d34d3d6f04943cbf5c43cf4
Denner <dennerramiror@gmail.com>
2022-09-20 22:19:34 -0300

    template string

---
+++ Javascript/fundamentos/templateStrings.js
@@ -0,0 +1,11 @@
+const nome = 'denner'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá
+    ${nome}!
+`
+
+console.log(concatenacao, template)
+
+// expressões
+console.log(`1 + 1 = ${1 + 1}`)
